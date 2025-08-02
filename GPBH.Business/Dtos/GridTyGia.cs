87e9dc99bf561dad38525089d0b54b2bfc84f089
thu.huynh <huynhthusoft97@gmail.com>
2025-08-02 22:46:59 +0700

    Update model

--- GPBH.Business/Dtos/GridTyGia.cs
+++ GPBH.Business/Dtos/GridTyGia.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GPBH.Business.Dtos
 {
     public class GridTyGia
     {
         public string Ma_nt { get; set; }               // Mã ngoại tệ
-        public DateTime Ngay_ap_dung { get; set; }     // Ngày áp dụng
-        public decimal Ty_gia { get; set; }             // Tỷ giá
+        public DateTime? Ngay_ap_dung { get; set; }     // Ngày áp dụng
+        public decimal? Ty_gia { get; set; }             // Tỷ giá
     }
 }
