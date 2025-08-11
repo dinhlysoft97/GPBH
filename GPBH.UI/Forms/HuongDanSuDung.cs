187c3e72517b80ef7bbc07bdb1308ac7abb0e55c
thu.huynh <huynhthusoft97@gmail.com>
2025-08-11 23:41:44 +0700

    fix dll

--- GPBH.UI/Forms/HuongDanSuDung.cs
+++ GPBH.UI/Forms/HuongDanSuDung.cs
@@ -10,15 +10,15 @@ namespace GPBH.UI.Forms
         {
             InitializeComponent();
 
-            var path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", "hdsd.pdf");
-            if (System.IO.File.Exists(path))
-            { 
-                webView21.Source = new Uri($"file:///{path.Replace("\\", "/")}");
-            }
-            else
-            {
-                MessageBox.Show("File hướng dẫn sử dụng không tồn tại.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
+            //var path = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Files", "hdsd.pdf");
+            //if (System.IO.File.Exists(path))
+            //{ 
+            //    webView21.Source = new Uri($"file:///{path.Replace("\\", "/")}");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("File hướng dẫn sử dụng không tồn tại.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            //}
         }
     }
 }
