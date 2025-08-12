c849d4150ba916a3324e1789f3085366580ea931
Thu, Huynh <thu.h@tripota.com.vn>
2025-08-12 09:15:08 +0700

    update code

--- GPBH.UI/Forms/DonHang1.cs
+++ GPBH.UI/Forms/DonHang1.cs
@@ -100,7 +100,6 @@ namespace GPBH.UI.Forms
 
             // Đặt thuộc tính KeyPreview của Form là true trong Designer hoặc trong code
             this.KeyPreview = true;
-            HandlerKeyF1();
             _dMQGService = dMQGService;
             _dMMTService = dMMTService;
             _dMHHService = dMHHService;
